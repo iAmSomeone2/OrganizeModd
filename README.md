ca282ed085d12db6042be6d879394d99c20d142c
Brenden Davidson <davidson.brenden15@gmail.com>
2019-12-02 15:46:26 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Origanize Modd
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/iAmSomeone2/organizemodd/modd"
+)
+
+const testFilePath string = "/home/bdavidson/Videos/Home_Videos/1-12-2010/20091224204921.modd"
+
+func main() {
+	moddFile, err := os.OpenFile(testFilePath, os.O_RDONLY, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileStats, _ := moddFile.Stat()
+
+	moddBytes := make([]byte, fileStats.Size())
+
+	numRead, err := moddFile.Read(moddBytes)
+	if err != nil {
+		log.Fatal(err)
+	} else if numRead != len(moddBytes) {
+		log.Print("Didn't fill byte array.")
+	}
+
+	moddFile.Close()
+
+	moddText := string(moddBytes)
+
+	moddData := modd.GetModd(moddText, testFilePath)
+
+	fmt.Println(moddData)
+}
